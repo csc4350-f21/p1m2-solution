9ef80ccb97d7014e815bd1d44e778e74534b02b9
jomart-gsu <jmartin191@gsu.edu>
2021-10-18 11:33:05 -0400

    Secret key change, DB URL setup

--- app.py
+++ app.py
@@ -10,10 +10,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = flask.Flask(__name__)
 # Point SQLAlchemy to your Heroku database
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+db_url = os.getenv("DATABASE_URL")
+if db_url.startswith("postgres://"):
+	db_url = db_url.replace("postgres://", "postgresql://", 1)
+app.config["SQLALCHEMY_DATABASE_URI"] = db_url
 # Gets rid of a warning
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.secret_key = b'I am a secret key!'  # don't defraud my app ok?
 
 from flask_login import UserMixin
 
